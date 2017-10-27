c1aa065afbb1224b31234eebcaf2afefce809934
Tobias Valbjørn <tvalbjoern@gmail.com>
2017-10-27 14:36:32 +0200

    Working prototype. We have a show_folder_content() that can show name, size and time when the file/folder was last modified.

--- src/exercise_13.cpp
+++ src/exercise_13.cpp
@@ -5,19 +5,18 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-#include <string>
 #include <iostream>
+
 #include <dirent.h>
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
 #include <string.h>
-#include <vector>
 
 using namespace std;
 
-const uint TIME_FMT = strlen("2012-12-31 12:59:59.123456789") + 1;
-vector<const char *> filenames;
 /*
  * Formats a struct timespec into a buffer.
  */
@@ -40,13 +39,21 @@ int timespec2str(char *buf, uint len, struct timespec *ts) {
 	return 0;
 }
 
-void get_folder_content(const char* path) {
+void show_folder_content(const char* path) {
+	const uint TIME_FMT = strlen("2012-12-31 12:59:59.123456789") + 1;
+	struct stat buf;
+	char timespec[TIME_FMT];
 	DIR *dir;
 	struct dirent *ent;
 	if ((dir = opendir(path)) != NULL) {
 		// print all the files and directories within directory
+		printf("     name\t      size (bytes)\t   last modified\n");
 		while ((ent = readdir(dir)) != NULL) {
-			filenames.push_back(ent->d_name);
+			stat(ent->d_name, &buf);
+			printf("%10s\t", ent->d_name);
+			printf("%10ld bytes\t", buf.st_size);
+			timespec2str(timespec, sizeof(timespec), &buf.st_mtim);
+			printf("%10s\n", timespec);
 		}
 		closedir(dir);
 	} else {
@@ -54,44 +61,12 @@ void get_folder_content(const char* path) {
 		perror("Could not open file");
 
 	}
-
-}
-
-char * append(const char * string1, char * string2)
-{
-    char * result = NULL;
-    asprintf(&result, "%s%s", string1, string2);
-    return result;
-}
-
-
-void show_folder_content(struct stat *buf, const char* path) {
-
-	char timespec[TIME_FMT];
-	string folder_name=path;
-	string temp;
-	for (int i=0 ; i < filenames.size() ; i++) {
-		string s =filenames[i];
-		temp="";
-		temp=folder_name+"/"+s;
-		cout << temp << endl;
-//		const char * temp2 = temp.c_str();
-//		stat(temp2, buf);
-//
-//		printf("file: %s\t\t\t\t", temp2);
-//		printf("Size: %ld bytes\t\t", buf->st_size);
-//		timespec2str(timespec, sizeof(timespec), &buf->st_mtim);
-//		printf("Time: %s\n", timespec);
-
-	}
 }
 
 int main(int argc, char ** argv) {
-	struct stat buf;
 
-	const char* path = "/home/Tobias/workspace/exercise_13";
-	get_folder_content(path);
-	show_folder_content(&buf, path);
-	return 0;
+const char* path = "/home/Tobias/workspace/exercise_13";
+show_folder_content(path);
+return 0;
 }
 
